3093d6c7f4febeeb6476e4d4ab76be0e89ece045
Anantadhirya <anantadhirya@gmail.com>
2024-09-21 15:23:10 +0700

    fix: cors origin

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./src/config/db');  
-const transactionRoutes = require('./src/routes/transactionRoutes');  // Pastikan path ini sesuai
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./src/config/db");
+const transactionRoutes = require("./src/routes/transactionRoutes"); // Pastikan path ini sesuai
+const cors = require("cors");
 
 // Memuat konfigurasi dari file .env
-dotenv.config(); 
+dotenv.config();
 
 // Menghubungkan ke MongoDB
 connectDB();
@@ -12,16 +13,23 @@ connectDB();
 // Membuat aplikasi Express
 const app = express();
 
+// CORS untuk mengizinkan permintaan dari berbagai origin
+app.use(
+  cors({
+    origin: true,
+  })
+);
+
 // Middleware untuk parsing JSON
 app.use(express.json());
 
 // Rute API
-app.use('/api', require('./src/routes/transactionRoutes'));
+app.use("/api", require("./src/routes/transactionRoutes"));
 
 // Menentukan port dari variabel lingkungan atau 5000 sebagai default
 const PORT = process.env.PORT || 5000;
 
 // Menjalankan server
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
