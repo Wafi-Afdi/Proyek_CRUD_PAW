3f6e1af6883c9f0acfb3c4e06913908aaf086071
Anantadhirya <anantadhirya@gmail.com>
2024-09-21 14:59:48 +0700

    fix: added 'name' in transactionSchema

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -1,31 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Membuat skema untuk transaksi
 const transactionSchema = new mongoose.Schema({
-    amount: {
-        type: Number,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    category: {
-        type: String,
-        required: true
-    },
-    type: {
-        type: String, // 'income' atau 'expense'
-        enum: ['income', 'expense'],
-        required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 // Membuat model Transaction dari schema
-const Transaction = mongoose.model('Transaction', transactionSchema);
+const Transaction = mongoose.model("Transaction", transactionSchema);
 
 module.exports = Transaction;
